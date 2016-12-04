d1c1db4227110fe485ba3bd3d739e1c09dbb2763
ghettovoice <ghettovoice@gmail.com>
2016-12-04 12:35:20 +0300

    disable psql output to fix build freezing

--- tests/test.sql
+++ tests/test.sql
@@ -8,12 +8,12 @@
 -- Started on 2016-12-04 12:27:51 MSK
 
 SET statement_timeout = 0;
--- SET lock_timeout = 0;
+SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
--- SET row_security = off;
+SET row_security = off;
 
 --
 -- TOC entry 1 (class 3079 OID 12395)
