60ec64983623b50c053f5f2eaef369cbbb668fcc
ghettovoice <ghettovoice@gmail.com>
2016-12-24 00:15:34 +0300

    Use postgres DB in connection settings

--- tests/pgmock.js
+++ tests/pgmock.js
@@ -14,7 +14,7 @@ export class ClientMock extends pg.Client {
   constructor () {
     super({
       user: 'postgres',
-      database: 'test'
+      database: 'postgres'
     })
 
     this.queries = []
